e1a352e5999d47072a925d3e5b34e9e07621bbca
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2026-08-04 15:25:09 +0900

    golangci-lint v2.12.2 and sacloud/textlint-action v0.1.0
    Signed-off-by: Kazumichi Yamamoto <yamamoto.febc@gmail.com>

--- includes/go/common.mk
+++ includes/go/common.mk
@@ -19,8 +19,8 @@ COPYRIGHT_YEAR          ?= 2022
 COPYRIGHT_FILES         ?= $$(find . -name "*.go" -print | grep -v "/vendor/")
 GO                      ?= go
 DEFAULT_GOALS           ?= fmt set-license go-licenses-check goimports lint test
-GOLANG_CI_LINT_VERSION  ?= v2.3.1
-TEXTLINT_ACTION_VERSION ?= v0.0.3
+GOLANG_CI_LINT_VERSION  ?= v2.12.2
+TEXTLINT_ACTION_VERSION ?= v0.1.0
 
 .DEFAULT_GOAL = default
 
@@ -42,7 +42,7 @@ dev-tools:
 	$(GO) install github.com/client9/misspell/cmd/misspell@latest
 	$(GO) install github.com/google/go-licenses@v1.0.0
 	$(GO) install github.com/rhysd/actionlint/cmd/actionlint@latest
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $$(go env GOPATH)/bin $(GOLANG_CI_LINT_VERSION)
+	curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $$(go env GOPATH)/bin $(GOLANG_CI_LINT_VERSION)
 
 .PHONY: goimports
 goimports: fmt
